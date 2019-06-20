4b6a2fa6d9408280716b95d0d768af3c05f8a491
pubblic <pubblic256@gmail.com>
2019-06-20 16:29:47 +0900

    전체적 인터페이스 수정

--- csv.go
+++ csv.go
@@ -1,32 +1,26 @@
 package csv
 
 import (
-	"bytes"
 	"encoding/csv"
 	"fmt"
 	"io"
 	"os"
 	"reflect"
 	"runtime"
-	"strings"
 	"sync"
 	"unsafe"
 
 	"golang.org/x/text/encoding"
 	"golang.org/x/text/encoding/htmlindex"
-	"golang.org/x/text/transform"
 )
+
+func getEncoding(charset string) (encoding.Encoding, error) {
+	return htmlindex.Get(charset)
+}
 
 type Opener struct {
 	// Name은 열 엑셀 파일 이름입니다.
 	Name string
-
-	// ColumnNames는 엑셀 파일 생성 시 파일의 첫 행에 쓰여집니다.
-	ColumnNames interface{}
-
-	Comma rune
-
-	UseCRLF bool
 
 	// Charset은 데이터 저장에 사용할 인코딩 이름을 설정합니다. 인코딩을
 	// 설정하지 않는다면 기본값인 utf-8을 사용합니다.
@@ -43,62 +37,61 @@ type Opener struct {
 	// 유용합니다.
 	Encoding encoding.Encoding
 
-	// Mode는 파일을 열거나 생성할 때 사용할 권한을 설정합니다. Mode 값을
+	// OpenFlag는 파일을 열 때 사용할 설정을 지정합니다.
+	OpenFlag int
+
+	// OpenMode는 파일을 열거나 생성할 때 사용할 권한을 설정합니다. Mode 값을
 	// 명시하지 않았다면 os.ModePerm 값을 기본으로 사용합니다.
-	Mode os.FileMode
+	OpenMode os.FileMode
+}
 
-	// Create가 설정되고 여는 파일이 없다면 파일을 새로 생성합니다.
-	Create bool
+func (o *Opener) getEncoding() (encoding.Encoding, error) {
+	if o.Encoding != nil {
+		return o.Encoding, nil
+	}
+	if o.Charset != "" {
+		return getEncoding(o.Charset)
+	}
+	return nil, nil
+}
 
-	// Append가 설정된다면 데이터 쓰기 시 기존 파일을 덮어씌우지 않고 아닌
-	// 파일 마지막에 추가합니다.
-	Append bool
-
-	// Sync가 설정된다면 데이터 쓰기를 버퍼를 거치지 않고 바로
-	// 파일시스템으로 보냅니다.
-	Sync bool
+func (o *Opener) openFile() (*os.File, error) {
+	return os.OpenFile(o.Name, o.OpenFlag, o.OpenMode)
 }
 
 // Open은 명시된 설정과 함께 엑셀 파일을 엽니다. 반환되는 *os.File 객체는
 // 자동으로 닫히지 않으며 호출자가 닫을 책임을 지닙니다.
-func (o Opener) Open() (*Writer, *os.File, error) {
-	openFlag := os.O_WRONLY
-	if o.Create {
-		openFlag |= os.O_CREATE
-	}
-	if o.Append {
-		openFlag |= os.O_APPEND
-	}
-	if o.Sync {
-		openFlag |= os.O_SYNC
-	}
-	openMode := o.Mode
-	if openMode == 0 {
-		openMode = os.ModePerm
-	}
-	if o.Charset != "" && o.Encoding == nil {
-		enc, err := htmlindex.Get(o.Charset)
-		if err != nil {
-			return nil, nil, err
-		}
-		o.Encoding = enc
-	}
-	raw, err := os.OpenFile(o.Name, openFlag, openMode)
+func (o Opener) Writer() (*Writer, *os.File, error) {
+	enc, err := o.getEncoding()
 	if err != nil {
 		return nil, nil, err
 	}
-	cw := csv.NewWriter(raw)
-	if o.Comma != 0 {
-		cw.Comma = o.Comma
+	raw, err := o.openFile()
+	if err != nil {
+		return nil, nil, err
 	}
-	cw.UseCRLF = o.UseCRLF
-	w := NewWriter(cw)
-	w.Encoding = o.Encoding
+	w := (io.Writer)(raw)
+	if enc != nil {
+		w = enc.NewEncoder().Writer(w)
+	}
+	return NewWriter(csv.NewWriter(w)), raw, nil
+}
 
-	if o.ColumnNames != nil && isEmptyFile(raw) {
-		w.Write(o.ColumnNames)
+// TODO
+func (o Opener) Reader() (*Reader, *os.File, error) {
+	enc, err := o.getEncoding()
+	if err != nil {
+		return nil, nil, err
 	}
-	return w, raw, nil
+	raw, err := o.openFile()
+	if err != nil {
+		return nil, nil, err
+	}
+	r := (io.Reader)(raw)
+	if enc != nil {
+		r = enc.NewDecoder().Reader(r)
+	}
+	return NewReader(csv.NewReader(r)), raw, nil
 }
 
 func isEmptyFile(raw *os.File) bool {
@@ -114,118 +107,26 @@ func IsEmptyFile(raw interface {
 }
 
 type Writer struct {
-	// Encoding은 데이터 쓰기 시 사용하는 인코딩입니다. 지정하지 않는다면
-	// 기본값인 utf-8이 사용됩니다. Encoding 필드는 Write, WriteAll, Fields
-	// 함수를 호출한 이후엔 읽기 전용이 됩니다.
-	Encoding encoding.Encoding
-
-	w *csv.Writer
-
-	enc        *encoding.Encoder
-	bufferPool sync.Pool
+	Writer     *csv.Writer
 	recordPool sync.Pool
 }
 
 func NewWriter(w *csv.Writer) *Writer {
 	return &Writer{
-		w: w,
+		Writer: w,
 	}
-}
-
-type borrowing struct {
-	src      []string
-	borrowed bool
-}
-
-func (w *Writer) encode(b borrowing) (borrowing, error) {
-	if w.isUTF8() {
-		return b, nil
-	}
-	dst := b.src
-	if !b.borrowed {
-		dst = w.borrowRecord(len(dst))
-	}
-	for i, s := range b.src {
-		var err error
-		dst[i], err = w.encodeString(s)
-		if err != nil {
-			w.recordPool.Put(dst)
-			return borrowing{}, err
-		}
-	}
-	return borrowing{dst, true}, nil
-}
-
-// assert: !w.isUTF8()
-func (w *Writer) encodeString(s string) (string, error) {
-	if w.enc == nil {
-		w.enc = w.Encoding.NewEncoder()
-	}
-	buf := w.getBuf()
-	buf.Reset()
-	defer w.bufferPool.Put(buf)
-	err := flood(transform.NewWriter(buf, w.enc.Transformer), unsafeBytes(s))
-	runtime.KeepAlive(s)
-	if err != nil {
-		return "", err
-	}
-	return buf.String(), nil
-}
-
-func flood(w io.WriteCloser, p []byte) error {
-	_, err := w.Write(p)
-	if err != nil {
-		w.Close()
-		return err
-	}
-	return w.Close()
-}
-
-// unsafe
-func unsafeBytes(s string) []byte {
-	repr := *(*reflect.SliceHeader)(unsafe.Pointer(&s))
-	repr.Cap = repr.Len
-	return *(*[]byte)(unsafe.Pointer(&repr))
-}
-
-func (w *Writer) getBuf() *bytes.Buffer {
-	buf, _ := w.bufferPool.Get().(*bytes.Buffer)
-	if buf == nil {
-		buf = new(bytes.Buffer)
-		buf.Grow(2048)
-	}
-	return buf
 }
 
 func (w *Writer) borrowRecord(size int) []string {
-	record, _ := w.recordPool.Get().([]string)
+	record, ok := w.recordPool.Get().([]string)
 	if cap(record) < size {
 		const e = 1
-		w.recordPool.Put(record)
+		if ok {
+			w.recordPool.Put(record)
+		}
 		record = make([]string, size+e)
 	}
 	return record[:size]
-}
-
-func (w *Writer) reclaimRecord(record []string) {
-	w.recordPool.Put(record)
-}
-
-func (w *Writer) reclaim(b borrowing) {
-	if b.borrowed {
-		w.reclaimRecord(b.src)
-	}
-}
-
-func (w *Writer) isUTF8() bool {
-	if w.Encoding == nil {
-		return true
-	}
-	name, err := htmlindex.Name(w.Encoding)
-	if err != nil {
-		return false
-	}
-	return strings.EqualFold(name, "utf-8")
 }
 
 // unsafe
@@ -238,10 +139,12 @@ func unsafeStrings(v reflect.Value) []string {
 	return *(*[]string)(unsafe.Pointer(&repr))
 }
 
-func (w *Writer) stringify(v reflect.Value) string {
+func stringifyField(v reflect.Value) string {
 	switch v.Kind() {
 	case reflect.String:
 		return v.String()
+	case reflect.Invalid:
+		return ""
 	default:
 		return fmt.Sprint(v)
 	}
@@ -256,26 +159,18 @@ func (w *Writer) Write(record interface{}) error {
 
 	// special case: equivalent to []string
 	if typ.Elem().Kind() == reflect.String {
-		b, err := w.encode(borrowing{src: unsafeStrings(v)})
-		if err != nil {
-			return err
-		}
-		err = w.w.Write(b.src)
-		w.reclaim(b)
+		err := w.Writer.Write(unsafeStrings(v))
 		runtime.KeepAlive(record)
 		return err
 	}
 
-	dst := w.borrowRecord(v.Len())
+	src := w.borrowRecord(v.Len())
 	for i := 0; i < v.Len(); i++ {
-		dst[i] = w.stringify(v.Index(i))
+		src[i] = stringifyField(v.Index(i))
 	}
-	defer w.reclaimRecord(dst)
-	b, err := w.encode(borrowing{dst, true})
-	if err != nil {
-		return err
-	}
-	return w.w.Write(b.src)
+	err := w.Writer.Write(src)
+	w.recordPool.Put(src)
+	return err
 }
 
 func (w *Writer) WriteAll(records []interface{}) error {
@@ -293,6 +188,17 @@ func (w *Writer) Fields(record ...interface{}) error {
 }
 
 func (w *Writer) Flush() error {
-	w.w.Flush()
-	return w.w.Error()
+	w.Writer.Flush()
+	return w.Writer.Error()
+}
+
+// TODO
+type Reader struct {
+	Reader *csv.Reader
+}
+
+func NewReader(r *csv.Reader) *Reader {
+	return &Reader{
+		Reader: r,
+	}
 }
