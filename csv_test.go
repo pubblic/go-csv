ee10b053b3e777b9e6f4ee22a96a8235408f1036
pubblic <pubblic256@gmail.com>
2019-06-24 14:29:28 +0900

    Writer 테스트 추가

--- csv_test.go
+++ csv_test.go
@@ -6,12 +6,71 @@ import (
 	"testing"
 )
 
-func writes(t *testing.T, records ...interface{}) *bytes.Buffer {
+var csvTests = []struct {
+	args []interface{}
+	want string
+}{
+	{nil, ""},
+	{
+		[]interface{}{
+			nil,
+		},
+		"\n",
+	},
+	{
+		[]interface{}{
+			[]string{"ab", "cd", "ef"},
+			[]interface{}{"hi", "jk", "lm", "no", 90},
+			[]string{"qr", "st"},
+		},
+		"ab,cd,ef\nhi,jk,lm,no,90\nqr,st\n",
+	},
+	{
+		[]interface{}{
+			[]int64{19, 23, 257},
+			[]interface{}{nil, "abc", nil, 22},
+			[]interface{}{stringType("hello"), nil},
+		},
+		"19,23,257\n,abc,,22\nhello,\n",
+	},
+	{
+		[]interface{}{
+			nil,
+			[]*int{nil, nil},
+			[]*string{},
+			[]interface{}{43, (*int)(nil), "abc"},
+		},
+		"\n,\n\n43,,abc\n",
+	},
+}
+
+type stringType string
+
+func TestWriter(t *testing.T) {
+	for i, tc := range csvTests {
+		buf, err := bufWrite(tc.args)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if buf.String() != tc.want {
+			t.Fatalf("Test#%d wants %s, but got %s",
+				i+1, tc.want, buf.String())
+		}
+	}
+}
+
+func bufWrite(args []interface{}) (*bytes.Buffer, error) {
 	buf := new(bytes.Buffer)
 	w := NewWriter(csv.NewWriter(buf))
-	check(t, w.WriteAll(records))
-	check(t, w.Flush())
-	return buf
+	err := w.WriteAll(args)
+	if err != nil {
+		return nil, err
+	}
+	err = w.Flush()
+	if err != nil {
+		return nil, err
+	}
+	return buf, nil
 }
 
 func check(t *testing.T, err error) {
@@ -20,28 +79,5 @@ func check(t *testing.T, err error) {
 	}
 }
 
-func TestWriter(t *testing.T) {
-	actual := writes(t, []interface{}{
-		"abcdef",
-		"ghijkl",
-		"mnopqr",
-		"stuvwx",
-		"yz",
-	}).String()
-	expected := "abcdef,ghijkl,mnopqr,stuvwx,yz\n"
-	if expected != actual {
-		t.Fatalf("expected %s, got %s", expected, actual)
-	}
-
-	actual = writes(t, []interface{}{
-		123,
-		456,
-		7,
-		"ab",
-		"def",
-	}).String()
-	expected = "123,456,7,ab,def\n"
-	if expected != actual {
-		t.Fatalf("expected %s, got %s", expected, actual)
 	}
 }
